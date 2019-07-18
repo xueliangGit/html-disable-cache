fb01274a06bcdcd17cde8607f8e7dc900f9c7347
xuxueliang <xu.xueliang@163.com>
2019-07-18 21:04:04 +0800

    edit

--- README.md
+++ README.md
@@ -1,65 +1,71 @@
-# html-disable-cache   [HDC]
+# html-disable-cache [HDC]
 
 [[English document]](./ENREADME.md)
 
 ![HDC version](https://img.shields.io/badge/HDC-0.2.5-brightgreen.svg)
 
-![HDC](https://raw.githubusercontent.com/xueliangGit/html-disable-cache/master/assets/hdc.jpg  "HDC")
+![HDC](https://raw.githubusercontent.com/xueliangGit/html-disable-cache/master/assets/hdc.jpg "HDC")
 
 ####
-HDC(html-disable-cache)你的html浏览器缓存的一个处理方案，为你解决浏览器的缓存的问题，让你的应用随你而变.
 
-这是一个处理html浏览器缓存的工具；不管你的页面跑在那个webview中，都会面临着缓存的考验，
+HDC(html-disable-cache)你的 html 浏览器缓存的一个处理方案，为你解决浏览器的缓存的问题，让你的应用随你而变.
+
+这是一个处理 html 浏览器缓存的工具；不管你的页面跑在那个 webview 中，都会面临着缓存的考验，
 让你的网页不能实时根据你更新的内容来显示；这个工具就是再各种浏览器缓存折么中诞生的；
 
 ---
+
 `已经集成了Cli，方便在非框架搭建的项目中使用`
 
 ---
+
 ## 该工具用法：
->该工具已集成Cli命令
 
- `HDC url [path]` path 是 html文件夹的目录，不传时默认是当前路径
+> 该工具已集成 Cli 命令
 
+`HDC url [path]` path 是 html 文件夹的目录，不传时默认是当前路径
 
- `HDC config [path]` path 是 HDC 的配置文件路径 不传时 将在当前目录下查找 `HDC.config.js` 文件进行读取。
+`HDC config [path]` path 是 HDC 的配置文件路径 不传时 将在当前目录下查找 `HDC.config.js` 文件进行读取。
 
-HDC配置文件如下
-````js
-const path = require('path')
-module.exports={
-  distPath:path.join(__dirname,'./dist'),
-  floderName:'mine',// js放在目录的名字 默认是 HDC
-  ignoreAttr:'hdc-ignore', //不需要hdc 处理的 js 默认是 hdc-ignore
-  removeIgnoreAttr:true // 是否在程序结束后 移除标识  默认是true
-}
-````
->使用Cli方法
+HDC 配置文件如下
 
+```js
+const path = require("path");
+module.exports = {
+  distPath: path.join(__dirname, "./dist"),
+  floderName: "mine", // js放在目录的名字 默认是 HDC
+  ignoreAttr: "hdc-ignore", //不需要hdc 处理的 js 默认是 hdc-ignore
+  removeIgnoreAttr: true // 是否在程序结束后 移除标识  默认是true
+};
+```
 
-全局安装 `html-disable-cache` 
+> 使用 Cli 方法
 
-````
+全局安装 `html-disable-cache`
+
+```
 npm i html-disable-cache -g
-````
- 
+```
 
-1、 在 `html` 目录下运行 `HDC url` 命令即可 
+1、 在 `html` 目录下运行 `HDC url` 命令即可
 
 2、 在项目目录添加`HDC`配置文件，见上面的文件信息；直接在目录下运行 `HDC config` 命令
 
->不使用Cli命令，要集成到当前项目
+> 不使用 Cli 命令，要集成到当前项目
 
 先安装依赖
-````
+
+```
 npm i html-disable-cache -S
-````
+```
+
 添加 `HDC.js` 文件
-````js
+
+```js
 // HDC.js
 // 用法
-let HDC =require('html-disable-cache')
-HDC('你的html目录的绝对路径 ');
+let HDC = require("html-disable-cache");
+HDC("你的html目录的绝对路径 ");
 /* or
 HDC({
   distPath:path.join(__dirname,'./dist'),
@@ -70,33 +76,40 @@ HDC({
 */
 // 例如
 
-let HDC =require('html-disable-cache')
-let path = require('path')
-HDC(path.join(__dirname,'./dist'));
-````
-运行 node命令可以
-````
+let HDC = require("html-disable-cache");
+let path = require("path");
+HDC(path.join(__dirname, "./dist"));
+```
+
+运行 node 命令可以
+
+```
 node HDC.js
-````
+```
 
 ---
->0.2.5 @HDC 2019-3-18  更新
->1. 优化加载，避免存在依赖时，无法进行正确顺序执行js；
->
->0.2.0 @HDC 2019-2-19  更新
->1. 优化错误，避免重复处理统一文件；
->2. 优化加载代码
->
->0.1.7 @HDC 2019-2-17  更新
->
->1. 调整加载js；使js加载完毕后恢复大致原来的位置；例如在head中的；加载完后在head中；在有id的div中的加载完后还在div中，其它请况全在body中；让html开发人员在找js位置时不会一头雾水。
->
 
-><br>本工具主要用的思想是：<br><br>
-加载静态资源时，
-例如js，css；若是地址不变，浏览器短时间内只会再第一次加载页面时区请求服务器上的文件，之后的时候会默认从缓存中获取，这是为了加快网页的访问；当地址改变时才会去调用服务器上的资源；<BR>
-这个为了让静态文件再有修改的时候去调用服务器的资源，没有修改的时候那就直接走缓存好了；<br>
-所以这个时候就需要一个方法去检测，到底该文件是否需要从新从服务器上加载；<br><br><br>
-原理：用到的工具-js文件<br><br>
-我用js去加载静态文件的方式来加载静态资源；在html中只有一个js加载资源的函数，可以动态设置资源链接例如 `src='./a.js?disCache=123'` ，并且页面加载完毕就用加载一个该页面公共的js，这个js的地址是动态赋值的，所他是不会缓存起来的（充当检测变化接口）；在这个js里就是用加载函数去加载需要加载的静态资源；到这里，具体怎么变就看你的了；<br><br>
+> 0.2.8 @HDC 2019-7-18 更新
+>
+> 1.  优化加载，若是加载 css，先加载 css 再加载 js；
 
+> 0.2.5 @HDC 2019-3-18 更新
+>
+> 1.  优化加载，避免存在依赖时，无法进行正确顺序执行 js；
+>
+> 0.2.0 @HDC 2019-2-19 更新
+>
+> 1.  优化错误，避免重复处理统一文件；
+> 2.  优化加载代码
+>
+> 0.1.7 @HDC 2019-2-17 更新
+>
+> 1.  调整加载 js；使 js 加载完毕后恢复大致原来的位置；例如在 head 中的；加载完后在 head 中；在有 id 的 div 中的加载完后还在 div 中，其它请况全在 body 中；让 html 开发人员在找 js 位置时不会一头雾水。
+
+> <br>本工具主要用的思想是：<br><br>
+> 加载静态资源时，
+> 例如 js，css；若是地址不变，浏览器短时间内只会再第一次加载页面时区请求服务器上的文件，之后的时候会默认从缓存中获取，这是为了加快网页的访问；当地址改变时才会去调用服务器上的资源；<BR>
+> 这个为了让静态文件再有修改的时候去调用服务器的资源，没有修改的时候那就直接走缓存好了；<br>
+> 所以这个时候就需要一个方法去检测，到底该文件是否需要从新从服务器上加载；<br><br><br>
+> 原理：用到的工具-js 文件<br><br>
+> 我用 js 去加载静态文件的方式来加载静态资源；在 html 中只有一个 js 加载资源的函数，可以动态设置资源链接例如 `src='./a.js?disCache=123'` ，并且页面加载完毕就用加载一个该页面公共的 js，这个 js 的地址是动态赋值的，所他是不会缓存起来的（充当检测变化接口）；在这个 js 里就是用加载函数去加载需要加载的静态资源；到这里，具体怎么变就看你的了；<br><br>
--- src/index.js
+++ src/index.js
@@ -7,154 +7,157 @@ const chalk = require('chalk')
 var minify = require('html-minifier').minify;
 // let this.conf.distPath =''// 项目目录  path.join(__dirname,'../../build')
 var conf = {
-  show:false,
-  floderName:'HDC'
+  show: false,
+  floderName: 'HDC'
 }
-let jsStr=fs.readFileSync(path.resolve(__dirname,'loadJsTem.js'), 'utf8');
-let log_=chalk.blueBright('Html-disable-cache:\n    ')
+let jsStr = fs.readFileSync(path.resolve(__dirname, 'loadJsTem.js'), 'utf8');
+let log_ = chalk.blueBright('Html-disable-cache:\n    ')
 /**
  * distPath 是静态文件html文件夹的路径
  * config{
  * floderName 放js的文件夹
  * }
  * */
-function HDC(distResolvePath,config={}){
-  let initConf = { 
-    htmlNum:0,
-    staticNum:0,
-    isDid:0}
+function HDC (distResolvePath, config = {}) {
+  let initConf = {
+    htmlNum: 0,
+    staticNum: 0,
+    isDid: 0
+  }
   this.conf = {
-    show:false,
-    distPath:'',
-    floderName:'HDC',
-    removeIgnoreAttr:true,
-    ignoreAttr:'hdc-ignore', // 排除处理的js或者css
-    doStyle:false
+    show: false,
+    distPath: '',
+    floderName: 'HDC',
+    removeIgnoreAttr: true,
+    ignoreAttr: 'hdc-ignore', // 排除处理的js或者css
+    doStyle: false
   }
-  if(typeof distResolvePath === 'string'){
-    this.conf.distPath=distResolvePath
-  }else if(typeof distResolvePath === 'object'){
-    Object.assign(this.conf,distResolvePath,initConf)
+  if (typeof distResolvePath === 'string') {
+    this.conf.distPath = distResolvePath
+  } else if (typeof distResolvePath === 'object') {
+    Object.assign(this.conf, distResolvePath, initConf)
   }
-  Object.assign(this.conf,config,initConf)
+  Object.assign(this.conf, config, initConf)
   if (!fs.pathExistsSync(this.conf.distPath)) {
-    console.log(log_,chalk.yellow('需要保证 '+this.conf.distPath+'目录存在'))
+    console.log(log_, chalk.yellow('需要保证 ' + this.conf.distPath + '目录存在'))
     process.exit(0)
   }
-  console.log(log_,chalk.yellow('HDC处理的目录是 '+this.conf.distPath))
-  var cache_html = getMultiEntry(this.conf.distPath+'/**/**/*.html'); // 获得入口js文件
+  console.log(log_, chalk.yellow('HDC处理的目录是 ' + this.conf.distPath))
+  var cache_html = getMultiEntry(this.conf.distPath + '/**/**/*.html'); // 获得入口js文件
   // console.log(cache_html)
-  this.conf.htmlNum=Object.keys(cache_html).length
-  disCache.call(this,cache_html);
+  this.conf.htmlNum = Object.keys(cache_html).length
+  disCache.call(this, cache_html);
 }
 //---------some fn
-function disCache(obj){
-  var j=0
-  for(let i in obj){
+function disCache (obj) {
+  var j = 0
+  for (let i in obj) {
     j++
     var html = fs.readFileSync(obj[i], 'utf8')
-    doHtml.call(this,html,obj[i],i,j)
+    doHtml.call(this, html, obj[i], i, j)
   }
-  if(!j){
-    console.log(log_,chalk.yellow('没有要处理的HTML文件,请确定路径输入正确'))
+  if (!j) {
+    console.log(log_, chalk.yellow('没有要处理的HTML文件,请确定路径输入正确'))
     process.exit(0);
   }
 }
 //-------- 处理html文件
-function doHtml(html,htmlUrl,baseName,htmlIndex){
+function doHtml (html, htmlUrl, baseName, htmlIndex) {
   let $ = cheerio.load(html); // 加载一个 html 文本 
-  if($('[hdc-did]').length){
-    console.log(log_,chalk.cyanBright(htmlUrl+':该文件已经被HDC处理了,若要更新请手动更新该文件加载的js中的参数'))
+  if ($('[hdc-did]').length) {
+    console.log(log_, chalk.cyanBright(htmlUrl + ':该文件已经被HDC处理了,若要更新请手动更新该文件加载的js中的参数'))
     return false
   }
-  let scripts=$(`script[src]:not([${this.conf.ignoreAttr}])`)
-  
-  // 处理js
-  let needLoadJs=[]
-  scripts.each(function(i,v){
-    let position = 'head'
-    let id = ''
-    if(v.parent.name=='head'){
-      position = 'head'
-    }else if(v.parent.name=='body'){
-      position = 'body'
-    }else if(v.parent.attribs.id){
-      position =''
-      id=v.parent.attribs.id
-    }else{
-      position = 'body'
-    }
-    needLoadJs.push({url:v.attribs.src,type:'js',position,id})
-  })
-  scripts.remove()
-  if(this.conf.doStyle){
+  let scripts = $(`script[src]:not([${ this.conf.ignoreAttr }])`)
+
+  let needLoadJs = []
+  // 处理css
+  if (this.conf.doStyle) {
     //处理 style
-    let styls=$(`link[rel="stylesheet"]:not([${this.conf.ignoreAttr}])`)
-    styls.each(function(i,v){
-      needLoadJs.push({url:v.attribs.href,type:'css',position:'head',id:''})
+    let styls = $(`link[rel="stylesheet"]:not([${ this.conf.ignoreAttr }])`)
+    styls.each(function (i, v) {
+      needLoadJs.push({url: v.attribs.href, type: 'css', position: 'head', id: ''})
     })
     styls.remove()
   }
-  if(this.conf.removeIgnoreAttr){
-    $(`script[${this.conf.ignoreAttr}]`).removeAttr(this.conf.ignoreAttr);
-    $(`link[rel="stylesheet"][${this.conf.ignoreAttr}]`).removeAttr(this.conf.ignoreAttr);
+  // 处理js
+  scripts.each(function (i, v) {
+    let position = 'head'
+    let id = ''
+    if (v.parent.name == 'head') {
+      position = 'head'
+    } else if (v.parent.name == 'body') {
+      position = 'body'
+    } else if (v.parent.attribs.id) {
+      position = ''
+      id = v.parent.attribs.id
+    } else {
+      position = 'body'
+    }
+    needLoadJs.push({url: v.attribs.src, type: 'js', position, id})
+  })
+  scripts.remove()
+
+  if (this.conf.removeIgnoreAttr) {
+    $(`script[${ this.conf.ignoreAttr }]`).removeAttr(this.conf.ignoreAttr);
+    $(`link[rel="stylesheet"][${ this.conf.ignoreAttr }]`).removeAttr(this.conf.ignoreAttr);
     // $(`script[${this.conf.ignoreAttr}]`,`link[rel="stylesheet"][${this.conf.ignoreAttr}]`).removeAttr(this.conf.ignoreAttr)
   }
-  let baseNameUrl=path.normalize(baseName,'/').replace(this.conf.distPath,'').split(path.sep).join('_')
-  let jsName = './'+this.conf.floderName+'/'+baseNameUrl+'.js'
+  let baseNameUrl = path.normalize(baseName, '/').replace(this.conf.distPath, '').split(path.sep).join('_')
+  let jsName = './' + this.conf.floderName + '/' + baseNameUrl + '.js'
   // jsName=path.relative(path.normalize(baseName,'../'),path.join(distPath,jsName))
   this.conf.staticNum += needLoadJs.length
-  if(needLoadJs.length){
-    this.conf.isDid ++
+  if (needLoadJs.length) {
+    this.conf.isDid++
     $('body').append(`
     <script type='text/javascript' language = 'javascript' hdc-did>
-    ${jsStr.replace("replaceUrl",path.relative(path.join(baseName,'../'),path.join(this.conf.distPath,jsName)).split(path.sep).join('/'))}
+    ${jsStr.replace("replaceUrl", path.relative(path.join(baseName, '../'), path.join(this.conf.distPath, jsName)).split(path.sep).join('/')) }
     </script>
     `)
-    let htmlData=minify($.html(),{removeComments: true,collapseWhitespace: true,minifyJS:true, minifyCSS:true})
-    writeHtml.call(this,htmlUrl,htmlData)
-    let jsData='__loadFn('+JSON.stringify(needLoadJs)+','+(+new Date())+');'
-    writJs.call(this,path.join(this.conf.distPath,jsName),jsData)
-  }else{
-    if(this.conf.isDid==0&&htmlIndex==this.conf.htmlNum){
-      console.log(log_,chalk.cyanBright('该文件夹下的HTML没有做处理；可能原因是：\n     1、没有引入外部JS、css \n     2、已经做过处理'))
+    let htmlData = minify($.html(), {removeComments: true, collapseWhitespace: true, minifyJS: true, minifyCSS: true})
+    writeHtml.call(this, htmlUrl, htmlData)
+    let jsData = '__loadFn(' + JSON.stringify(needLoadJs) + ',' + (+new Date()) + ');'
+    writJs.call(this, path.join(this.conf.distPath, jsName), jsData)
+  } else {
+    if (this.conf.isDid == 0 && htmlIndex == this.conf.htmlNum) {
+      console.log(log_, chalk.cyanBright('该文件夹下的HTML没有做处理；可能原因是：\n     1、没有引入外部JS、css \n     2、已经做过处理'))
     }
   }
 }
-function writJs(jsPath,data){
+function writJs (jsPath, data) {
   // console.log(jsPath)
-  fs.exists(path.dirname(jsPath),(exists)=>{
-    if(exists){
-      _wJs.call(this,jsPath,data)
-    }else{
-      mkDirSync(this.conf.distPath,path.relative(this.conf.distPath,path.dirname(jsPath)))
-      _wJs.call(this,jsPath,data)
+  fs.exists(path.dirname(jsPath), (exists) => {
+    if (exists) {
+      _wJs.call(this, jsPath, data)
+    } else {
+      mkDirSync(this.conf.distPath, path.relative(this.conf.distPath, path.dirname(jsPath)))
+      _wJs.call(this, jsPath, data)
     }
   })
 }
-function _wJs(jsPath,data){
-  fs.writeFile(jsPath,data,(err)=>{
-    if(err) throw err;
-    if(this.conf.show){
-      console.log(log_,chalk.green(jsPath+'文件已建立'));
+function _wJs (jsPath, data) {
+  fs.writeFile(jsPath, data, (err) => {
+    if (err) throw err;
+    if (this.conf.show) {
+      console.log(log_, chalk.green(jsPath + '文件已建立'));
     }
     this.conf.staticNum--
-    if(this.conf.staticNum===0){
+    if (this.conf.staticNum === 0) {
       console.log()
-      console.log(log_,chalk.green(' js文件已建立'));
+      console.log(log_, chalk.green(' js文件已建立'));
     }
   });
 }
-function writeHtml (filePath,data) {
-  fs.writeFile(filePath,data, (err)=>{
-    if(err) throw err;
-    if(this.conf.show){
-      console.log(log_,chalk.green(filePath+'文件处理完毕'));
+function writeHtml (filePath, data) {
+  fs.writeFile(filePath, data, (err) => {
+    if (err) throw err;
+    if (this.conf.show) {
+      console.log(log_, chalk.green(filePath + '文件处理完毕'));
     }
     this.conf.htmlNum--
-    if(this.conf.htmlNum===0){
+    if (this.conf.htmlNum === 0) {
       console.log()
-      console.log(log_,chalk.green(' html文件处理完毕'));
+      console.log(log_, chalk.green(' html文件处理完毕'));
     }
   })
 }
@@ -182,33 +185,34 @@ function getMultiEntry (globPath) {
     // tmp = entry.split('/').splice(-4)
     // 优化 多层级页面 路径获取错误
     tmp = entry.split('/')
-  var stat = fs.lstatSync(entry);
-  if(!stat.isDirectory()){;// true || false 判断是不是文件夹
-    tmp.splice(tmp.length - 1)
-    // console.log(entry, tmp)
-    var pathsrc =tmp.join('/')
-    if (tmp[0] == 'src') {
-      tmp.shift()
-      pathsrc = tmp.join('/')
+    var stat = fs.lstatSync(entry);
+    if (!stat.isDirectory()) {
+      ;// true || false 判断是不是文件夹
+      tmp.splice(tmp.length - 1)
+      // console.log(entry, tmp)
+      var pathsrc = tmp.join('/')
+      if (tmp[0] == 'src') {
+        tmp.shift()
+        pathsrc = tmp.join('/')
+      }
+      // 优化 完毕
+      // console.log(pathsrc)
+      pathname = pathsrc + '/' + basename // 正确输出js和html的路径
+      entries[pathname] = entry
+      // console.log(pathname + '-----------' + entry)
     }
-    // 优化 完毕
-    // console.log(pathsrc)
-    pathname = pathsrc + '/' + basename // 正确输出js和html的路径
-    entries[pathname] = entry
-    // console.log(pathname + '-----------' + entry)
-  }
   })
   return entries
 }
 // 处理js
-function getUgJs(str){
-  return str.replace(/function /g,'__FUN__').replace(/var /g,'__VAR__').replace(/let /g,'__LET__').replace(/const /g,'__CONST__').replace(/else if/g,'__ELSEIF__').replace(/typeof /g,'__TYPEOF__').replace(/\n/g,';').replace(/ /g,'').replace(/{;/g,'{').replace(/;}/g,'}').replace(/__FUN__/g,'function ').replace(/__VAR__/g,'var ').replace(/__LET__/g,'let ').replace(/__CONST__/g,'const ').replace(/__ELSEIF__/g,'else if').replace(/__TYPEOF__/g,'typeof ')
+function getUgJs (str) {
+  return str.replace(/function /g, '__FUN__').replace(/var /g, '__VAR__').replace(/let /g, '__LET__').replace(/const /g, '__CONST__').replace(/else if/g, '__ELSEIF__').replace(/typeof /g, '__TYPEOF__').replace(/\n/g, ';').replace(/ /g, '').replace(/{;/g, '{').replace(/;}/g, '}').replace(/__FUN__/g, 'function ').replace(/__VAR__/g, 'var ').replace(/__LET__/g, 'let ').replace(/__CONST__/g, 'const ').replace(/__ELSEIF__/g, 'else if').replace(/__TYPEOF__/g, 'typeof ')
   //恢复 特殊字符
 }
-module.exports=function(...arry){
-  try{
+module.exports = function (...arry) {
+  try {
     new HDC(...arry)
-  }catch(e){
+  } catch (e) {
     console.log(e)
   }
 }
